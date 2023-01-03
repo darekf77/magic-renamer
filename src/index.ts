36a88e831a5036c9956f988e729d8216a210607b
Dariusz Filipiak <dariusz.filipiak@igt.com>
2023-01-03 07:28:12 +0100

    a

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export * from './lib';
-      
+//#region @backend
+export { run } from './start.backend';
+//#endregion
--- src/start.backend.ts
+++ src/start.backend.ts
@@ -1,6 +1,10 @@
 import { MagicRenamer } from './lib/magic-renamer';
 
 export async function run(args: string[]) {
+  if(args.join() === 'version') {
+    console.log('Magic renamer version: v0.0.0 !')
+    process.exit(0)
+  }
   const ins = MagicRenamer.Instance();
   ins.start(args.join(' '));
   process.exit(0)
