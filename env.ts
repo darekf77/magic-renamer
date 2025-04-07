44db0398fcb267becad47642c7686c0fbe6f097d
Dariusz Filipiak <darekf77@gmail.com>
2025-04-07 16:29:51 +0200

    release: v18.0.29

---
+++ env.ts
@@ -0,0 +1,9 @@
+import type { EnvOptions } from 'tnp/src';
+
+const env: Partial<EnvOptions> = {
+  website: {
+    domain: 'magic-renamer.example.domain.com',
+    title: 'Magic Renamer',
+  },
+};
+export default env;
