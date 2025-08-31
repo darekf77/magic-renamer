3f7f55c8405c3840007a893456309e90f9e12f4d
Dariusz Filipiak <darekf77@gmail.com>
2025-08-31 20:57:08 +0200

    release: v19.0.48

--- src/app.ts
+++ src/app.ts
@@ -4,16 +4,21 @@
 import { NgModule } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-magic-renamer',
   template: 'hello from magic-renamer',
-  styles: [` body { margin: 0px !important; } `],
+  styles: [
+    `
+      body {
+        margin: 0px !important;
+      }
+    `,
+  ],
 })
 export class MagicRenamerComponent implements OnInit {
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() { }
+  ngOnInit() {}
 }
 
 @NgModule({
@@ -22,16 +27,13 @@ export class MagicRenamerComponent implements OnInit {
   declarations: [MagicRenamerComponent],
   providers: [],
 })
-export class MagicRenamerModule { }
+export class MagicRenamerModule {}
 //#endregion
-
 
 async function start(port: number) {
   console.log('hello world');
 }
 
 export default start;
-
-
 
 //#endregion
